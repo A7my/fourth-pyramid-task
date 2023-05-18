dbe9a4be60943bde30cb0ec1f2af5a710c319809
Mohamed Azmy <85401829+A7my@users.noreply.github.com>
2023-05-18 05:37:08 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,39 +2,53 @@
 
 namespace App\Models;
 
-use App\Models\Product;
-use App\Models\Category;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Order;
+use App\Models\Service;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
 
+    public function order(){
+        return $this->belongsToMany(Order::class);
+    }
+
+    public function service(){
+        return $this->hasMany(Service::class , 'user_id');
+    }
     protected $fillable = [
         'name',
         'email',
         'password',
-        'role'
     ];
 
-
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
+        'remember_token',
     ];
 
-    public function product(){
-        return $this->belongsToMany(Product::class);
-    }
-
-    public function category(){
-        return $this->belongsToMany(Category::class);
-    }
-
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
-
